58bc357186fba05c4abba228d1ebcfc3393d4231
Solareon <769465+solareon@users.noreply.github.com>
2024-07-10 22:00:05 +0200

    refactor(client/main): fix structure and move config out of client lua (#2)
    * refactor(client/main): fix structure and move config out of client lua
    
    * chore(client/main): add descriptive comments

---
+++ config/client.lua
@@ -0,0 +1,11 @@
+return {
+    -- Density of various things in the world
+    -- Minimum value is 0.0, maximum value is 1.0
+    -- Value of 1.0 represents GTA Online populate rates
+    -- Value of 0.0 removes all of that type
+    parked = 0.8, -- Density of parked vehicles
+    vehicle = 0.8, -- Density of vehicles
+    randomvehicles = 0.8, -- Density of random vehicles
+    peds = 0.8, -- Density of random peds (civilians, etc.)
+    scenario = 0.8, -- Density of scenario peds (bikers, gangsters, etc.)
+}
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,14 @@ description 'Population management'
 repository 'https://github.com/Qbox-project/qbx_density'
 version '1.0.0'
 
-client_script 'main.lua'
+client_scripts {
+    '@ox_lib/init.lua',
+    'client/main.lua'
+}
+
+files {
+    'config/client.lua'
+}
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
