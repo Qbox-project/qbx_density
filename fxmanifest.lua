8f70cf14cb762587eec5b8178e1574f3b233cb8d
solareon <solareon@users.noreply.github.com>
2024-07-11 10:56:50 +0000

    chore: bump manifest version to v1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
-version '1.0.0'
+version '1.0.1'
 
 name 'qbx_density'
 description 'Population management'
 repository 'https://github.com/Qbox-project/qbx_density'
-version '1.0.0'
+version '1.0.1'
 
 client_scripts {
     '@ox_lib/init.lua',
